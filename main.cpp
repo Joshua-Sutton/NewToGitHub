d2fec1957d2c7ffa9b1c1dde17d8c5033ffcae78
Joshua Sutton <joshuajaysutton@gmail.com>
2021-06-09 10:52:12 -0500

    New file and text added
    Basic cout statements and some text

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "New Project" << endl;
+    cout << "Testing Github" << endl;
+    cout << "Working to be like a software dev" << endl;
+}
